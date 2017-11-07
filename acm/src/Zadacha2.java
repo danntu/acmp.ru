d91643caaac301c7f1e03f1d548e9e04aae2ca5c
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2017-11-07 16:24:50 +0600

    Задача 2. Сумма

--- acm/src/Zadacha2.java
+++ acm/src/Zadacha2.java
@@ -1,15 +1,20 @@
 import java.io.*;
 import java.util.*;
 
-public class Zadacha1 {
+public class Zadacha2 {
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
 
         int a = in.nextInt();
-        int b = in.nextInt();
 
-        out.println(a + b);
+
+
+        if(a<=0){
+            out.print((1+a)*(Math.abs(a)+2)/2 );
+        }
+        else
+            out.print((1+a)*a/2);
 
         out.flush();
     }
